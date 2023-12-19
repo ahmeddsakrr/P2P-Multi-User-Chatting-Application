715a1e0296b7d3f923073eb262842887f2cce252
ahmeddsakrr <91761423+ahmeddsakrr@users.noreply.github.com>
2023-12-19 08:42:50 +0200

    Added Unit Tests and DocStrings for the DatabaseAccess Class

--- DAO/database_access.py
+++ DAO/database_access.py
@@ -1,54 +1,175 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 
 
 class DatabaseAccess:
 
     def __init__(self):
-        self.client = MongoClient('localhost', 27017)
-        self.db = self.client['p2p_database']
+        try:
+
+            self.client = MongoClient('localhost', 27017)
+            self.db = self.client['p2p_database']
+        except errors.ConnectionFailure as e:
+            print("Could not connect to MongoDB: %s" % e)
 
     def get_db(self):
+        """
+        Returns the MongoDB database object.
+        """
         return self.db
 
     def get_client(self):
+        """
+        Returns the MongoClient object for the database connection.
+        """
         return self.client
 
     def user_exists(self, username):
-        return self.db.user.find_one({'username': username}) is not None
+        """
+            Checks if a user exists in the database.
+
+            Parameters:
+            - username (str): The username to check.
+
+            Returns:
+            - bool: True if the user exists, False otherwise.
+        """
+        try:
+
+            return self.db.user.find_one({'username': username}) is not None
+        except errors.PyMongoError as e:
+            print("Error checking user existence : %s" % e)
 
     def create_user(self, username, password):
-        if not self.user_exists(username):
-            self.db.user.insert_one({'username': username, 'password': password})
-            return True
-        return False
+        """
+        Creates a new user in the database.
+
+        Parameters:
+        - username (str): The username of the new user.
+        - password (str): The (hashed) password of the new user.
+
+        Returns:
+        - bool: True if the user is created successfully, False if the user already exists.
+        """
+        try:
+
+            if not self.user_exists(username):
+                self.db.user.insert_one({'username': username, 'password': password})
+                return True
+        except errors.PyMongoError as e:
+            print("Error creating user : %s" % e)
+            return False
 
     def get_user(self, username):
+        """
+        Retrieves user information from the database.
+
+        Parameters:
+        - username (str): The username of the user to retrieve.
+
+        Returns:
+        - Object: User information if found, else None.
+        """
         return self.db.user.find_one({'username': username})
 
     def get_password(self, username):
+        """
+        Retrieves the password of a user from the database.
+
+        Parameters:
+        - username (str): The username of the user.
+
+        Returns:
+        - str: The password of the user.
+        """
         return self.db.user.find_one({'username': username})['password']
 
     def set_password(self, username, password):
+        """
+        Updates the password of a user in the database.
+
+        Parameters:
+        - username (str): The username of the user.
+        - password (str): The new password to set.
+        """
         self.db.user.update_one({'username': username}, {'$set': {'password': password}})
 
     def is_user_online(self, username):
+        """
+        Checks if a user is currently online.
+
+        Parameters:
+        - username (str): The username of the user.
+
+        Returns:
+        - bool: True if the user is online, False otherwise.
+        """
         return self.db.online_users.find_one({'username': username}) is not None
 
     def set_user_online(self, username, port, ip):
-        if not self.is_user_online(username):
-            self.db.online_users.insert_one({'username': username, 'port': port, 'ip': ip})
+        """
+        Sets a user as online in the database.
+
+        Parameters:
+        - username (str): The username of the user.
+        - port (int): The port number of the user.
+        - ip (str): The IP address of the user.
+        """
+        try:
+            if not self.is_user_online(username):
+                self.db.online_users.insert_one({'username': username, 'port': port, 'ip': ip})
+        except errors.PyMongoError as e:
+            print("Error setting user online : %s" % e)
 
     def set_user_offline(self, username):
-        self.db.online_users.delete_one({'username': username})
-    
+        """
+        Sets a user as offline in the database.
+
+        Parameters:
+        - username (str): The username of the user.
+        """
+        try:
+            self.db.online_users.delete_one({'username': username})
+        except errors.PyMongoError as e:
+            print("Error setting user offline : %s" % e)
+
     def get_user_ip(self, username):
+        """
+        Retrieves the IP address of a user from the database.
+
+        Parameters:
+        - username (str): The username of the user.
+
+        Returns:
+        - str: The IP address of the user.
+        """
         return self.db.online_users.find_one({'username': username})['ip']
     
     def get_user_port(self, username):
+        """
+        Retrieves the port number of a user from the database.
+
+        Parameters:
+        - username (str): The username of the user.
+
+        Returns:
+        - int: The port number of the user.
+        """
         return self.db.online_users.find_one({'username': username})['port']
     
     def get_online_users(self):
+        """
+        Retrieves all online users from the database.
+
+        Returns:
+        - pymongo.cursor.Cursor: Cursor object containing online users' information.
+        """
         return self.db.online_users.find()
     
     def get_online_users_list(self):
+        """
+        Retrieves a list of usernames of all online users.
+
+        Returns:
+        - list: List of usernames of online users.
+        """
         return [user['username'] for user in self.get_online_users()]
---
+++ Tests/TestDatabaseAccess.py
@@ -0,0 +1,88 @@
+import unittest
+from DAO import DatabaseAccess  
+
+class TestDatabaseAccess(unittest.TestCase):
+
+    def setUp(self):
+        # Initialize DatabaseAccess for testing
+        self.db_access = DatabaseAccess()
+
+    def tearDown(self):
+        # Clean up any resources after tests are run
+        # For example, delete test users created during testing
+        self.db_access.db.user.delete_many({'username': {'$in': ['test_user', 'new_user']}})
+        self.db_access.db.online_users.delete_many({'username': {'$in': ['test_user', 'new_user']}})
+
+    def test_user_exists(self):
+        # Test user existence when user exists
+        self.assertTrue(self.db_access.create_user('test_user', 'password'))
+        self.assertTrue(self.db_access.user_exists('test_user'))
+
+        # Test user existence when user doesn't exist
+        self.assertFalse(self.db_access.user_exists('non_existing_user'))
+
+    def test_create_user(self):
+        # Test creating a new user
+        self.assertTrue(self.db_access.create_user('new_user', 'password'))
+
+        # Test creating an existing user
+        self.assertFalse(self.db_access.create_user('test_user', 'password'))
+
+    def test_get_user(self):
+        # Test retrieving an existing user
+        self.assertIsNotNone(self.db_access.get_user('test_user'))
+
+        # Test retrieving a non-existing user
+        self.assertIsNone(self.db_access.get_user('non_existing_user'))
+
+    def test_get_password(self):
+        # Test retrieving password for an existing user
+        self.assertEqual(self.db_access.get_password('test_user'), 'password')
+
+        # Test retrieving password for a non-existing user
+        self.assertRaises(KeyError, self.db_access.get_password, 'non_existing_user')
+
+
+    def test_set_password(self):
+        # Test setting password for an existing user
+        self.db_access.create_user('test_user', 'old_password')
+        self.db_access.set_password('test_user', 'new_password')
+        self.assertEqual(self.db_access.get_password('test_user'), 'new_password')
+
+    def test_user_status(self):
+        # Test when user is online
+        self.db_access.set_user_online('test_user', 8080, '127.0.0.1')
+        self.assertTrue(self.db_access.is_user_online('test_user'))
+
+        # Test when user is offline
+        self.db_access.set_user_offline('test_user')
+        self.assertFalse(self.db_access.is_user_online('test_user'))
+
+    def test_get_user_ip(self):
+        # Test retrieving IP for an existing user
+        self.db_access.set_user_online('test_user', 8080, '127.0.0.1')
+        self.assertEqual(self.db_access.get_user_ip('test_user'), '127.0.0.1')
+
+        # Test retrieving IP for a non-existing user
+        self.assertIsNone(self.db_access.get_user_ip('non_existing_user'))
+
+    def test_get_user_port(self):
+        # Test retrieving port for an existing user
+        self.db_access.set_user_online('test_user', 8080, '127.0.0.1')
+        self.assertEqual(self.db_access.get_user_port('test_user'), 8080)
+
+        # Test retrieving port for a non-existing user
+        self.assertIsNone(self.db_access.get_user_port('non_existing_user'))
+
+    def test_get_online_users_list(self):
+        # Test retrieving list of online users
+        self.db_access.set_user_online('test_user', 8080, '127.0.0.1')
+        self.db_access.set_user_online('new_user', 9090, '192.168.1.1')
+        expected_users = ['test_user', 'new_user']
+        self.assertCountEqual(self.db_access.get_online_users_list(), expected_users)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
