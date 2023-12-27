1485d55be7529e3eb823e75f6467afac7e4619f0
Lailaehab <133366451+Lailaehab@users.noreply.github.com>
2023-12-27 19:49:36 +0200

    Update TestDatabaseAccess.py

--- Tests/TestDatabaseAccess.py
+++ Tests/TestDatabaseAccess.py
@@ -12,6 +12,7 @@ class TestDatabaseAccess(unittest.TestCase):
         # For example, delete test users created during testing
         self.db_access.db.user.delete_many({'username': {'$in': ['test_user', 'new_user']}})
         self.db_access.db.online_users.delete_many({'username': {'$in': ['test_user', 'new_user']}})
+        self.db_access.db.rooms.delete_many({'room_name': {'$in': ['test_room', 'new_room']}})
 
     def test_user_exists(self):
         # Test user existence when user exists
@@ -82,6 +83,41 @@ class TestDatabaseAccess(unittest.TestCase):
         self.db_access.set_user_online('new_user', 9090, '192.168.1.1')
         expected_users = ['test_user', 'new_user']
         self.assertCountEqual(self.db_access.get_online_users_list(), expected_users)
+
+    def test_get_chat_rooms(self):
+        # Test retrieving chat rooms when there are existing rooms
+        self.db_access.create_chat_room('test_room')
+        self.db_access.create_chat_room('new_room')
+        expected_rooms = ['test_room', 'new_room']
+        self.assertCountEqual(self.db_access.get_chat_rooms(), expected_rooms)
+
+        # Test retrieving chat rooms when there are no rooms
+        self.assertEqual(self.db_access.get_chat_rooms(), [])
+
+    def test_chat_room_exists(self):
+        # Test checking chat room existence when the room exists
+        self.db_access.create_chat_room('test_room')
+        self.assertTrue(self.db_access.chat_room_exists('test_room'))
+
+        # Test checking chat room existence when the room doesn't exist
+        self.assertFalse(self.db_access.chat_room_exists('non_existing_room'))
+
+    def test_join_chat_room(self):
+        # Test joining an existing chat room
+        self.db_access.create_chat_room('test_room')
+        self.assertTrue(self.db_access.join_chat_room('test_room', 'test_user'))
+
+        # Test joining a non-existing chat room
+        self.assertFalse(self.db_access.join_chat_room('non_existing_room', 'test_user'))
+
+    def test_create_chat_room(self):
+        # Test creating a new chat room
+        self.assertTrue(self.db_access.create_chat_room('new_room'))
+
+        # Test creating an existing chat room
+        self.assertFalse(self.db_access.create_chat_room('new_room'))
+
+
 
 
 if __name__ == '__main__':
