964702aaad88e1f444d093c681e18917b63c5f79
ahmeddsakrr <91761423+ahmeddsakrr@users.noreply.github.com>
2023-12-30 11:47:39 +0200

    Progress for Phase 3

---
+++ Network/PeerClient.py
@@ -0,0 +1,144 @@
+from socket import *
+import threading
+import time
+import select
+import logging
+from Service.color_utils import colored_print
+import colorama
+from PeerServer import PeerServer
+
+class PeerClient(threading.Thread):
+    def __init__(self, connected_ip, connected_port, username, peer_server : PeerServer, received_response, choice, room_id, room_peers : list, server_name = "192.168.107"):
+
+        threading.Thread.__init__(self)
+        self.server_name = server_name
+        self.server_port = 15600
+        self.connected_ip = connected_ip
+        self.connected_port = connected_port
+        self.username = username
+        self.tcp_socket = socket(AF_INET, SOCK_STREAM)
+        self.udp_socket = socket(AF_INET, SOCK_DGRAM)
+        self.peer_server = peer_server
+        # keeps the phrase that is used when creating the client
+        # if the client is created with a phrase, it means this one received the request
+        # this phrase should be none if this is the client of the requester peer
+        self.received_response = received_response
+        self.is_end = False
+        self.choice = choice #flag to indicate room or normal chat
+        self.room_id = room_id
+        self.room_peers = room_peers
+        self.is_room_empty = False
+
+
+    def update_peers(self):
+        message = "update-peers " + str(self.room_id)
+        self.tcp_socket.send(message.encode())
+        logging.info("Send to " + self.server_name + ":" + str(self.server_port) + " : " + message)
+        response = self.tcp_socket.recv(1024).decode()
+        updated_list_start = response.index("[")
+        updated_list_end = response.index("]") + 1
+        updated_list = response[updated_list_start:updated_list_end]
+        response = response.split()
+        self.room_peers = eval(updated_list)
+
+    def exit(self):
+        removed_port = self.peer_server.chat_room_server_port
+        request = "leave-room" + " " + str(self.room_id) + " " + str(removed_port)
+        self.tcp_socket.send(request.encode())
+        response = self.tcp_socket.recv(1024).decode()
+        return response
+
+    def run(self) -> None:
+        if self.choice == '5':
+            colored_print("Peer client started", "success")
+            colored_print("Connected to " + self.connected_ip + ":" + str(self.connected_port), "success")
+            self.tcp_socket.connect((self.connected_ip, self.connected_port))
+            if not self.peer_server.isChatting and self.received_response is None:
+                # if the server of this peer is not connected by someone else and if this is the requester side peer client then enters here
+                request_message = "create-private-chat-request " + self.username + " " + str(self.peer_server.peer_server_port)
+                logging.info("Send to " + self.connected_ip + ":" + str(self.connected_port) + " : " + request_message)
+
+                self.tcp_socket.send(request_message.encode())
+                colored_print("Waiting for response...", "success")
+                self.received_response = self.tcp_socket.recv(1024).decode()
+                logging.info("Received message: " + self.received_response + " from " + self.connected_ip + ":" + str(self.connected_port))
+                colored_print("Received response: " + self.received_response, "success")
+                self.received_response = self.received_response.split()
+                if self.received_response == "accept-connection":
+                    # if response is ok then incoming messages will be evaluated as client messages and will be sent to the connected server
+                    self.peer_server.isChatting = True
+                    self.peer_server.connected_peer_username = self.received_response[1]
+                    while self.peer_server.isChatting:
+                        # as long as the server status is chatting, this client can send messages
+                        sent_message = input("You: ")
+                        self.tcp_socket.send(sent_message.encode())
+                        logging.info("Send to " + self.connected_ip + ":" + str(self.connected_port) + " : " + sent_message)
+                        if sent_message == "exit":
+                            self.peer_server.isChatting = False
+                            self.peer_server.connected_peer_username = None
+                            self.is_end = True
+                            colored_print("Chat ended.", "success")
+                            break
+
+                    if not self.peer_server.isChatting:
+                        # if peer is not chatting, checks if this is not the ending side
+                        if not self.is_end:
+                            # tries to send a quit message to the connected peer
+                            # logs the message and handles the exception
+                            try:
+                                self.tcp_socket.send("exit".encode())
+                                logging.info("Send to " + self.connected_ip + ":" + str(self.connected_port) + " : " + "exit")
+                            except OSError as e:
+                                colored_print("Error: " + str(e), "error")
+                            self.tcp_socket.close()
+                            self.received_response = None
+
+                elif self.received_response[0] == "reject-connection":
+                    # if the response is reject then the client will be closed
+                    colored_print("Connection rejected.", "error")
+                    self.tcp_socket.send("reject-connection".encode())
+                    logging.info("Send to " + self.connected_ip + ":" + str(self.connected_port) + " : " + "reject-connection")
+                    self.tcp_socket.close()
+                elif self.received_response[0] == "reject-connection":
+                    colored_print("Peer Rejected the Connection", "error")
+                    self.tcp_socket.close()
+            elif self.received_response.split()[0] == "accept-connection":
+                self.peer_server.isChatting = True
+                response_sent = "accept-connection"
+                self.tcp_socket.send(response_sent.encode())
+                logging.info("Send to " + self.connected_ip + ":" + str(self.connected_port) + " : " + response_sent)
+                colored_print("Private chat request accepted.", "success")
+                while self.peer_server.isChatting:
+                    # as long as the server status is chatting, this client can send messages
+                    sent_message = input("You: ")
+                    self.tcp_socket.send(sent_message.encode())
+                    logging.info("Send to " + self.connected_ip + ":" + str(self.connected_port) + " : " + sent_message)
+                    if sent_message == "exit":
+                        self.peer_server.isChatting = False
+                        self.peer_server.connected_peer_username = None
+                        self.is_end = True
+                        colored_print("Chat ended.", "success")
+                        break
+
+                if not self.peer_server.isChatting:
+                    # if peer is not chatting, checks if this is not the ending side
+                    if not self.is_end:
+                        # tries to send a quit message to the connected peer
+                        # logs the message and handles the exception
+                        try:
+                            self.tcp_socket.send("exit".encode())
+                            logging.info("Send to " + self.connected_ip + ":" + str(self.connected_port) + " : " + "exit")
+                        except OSError as e:
+                            colored_print("Error: " + str(e), "error")
+                        self.tcp_socket.close()
+                        self.received_response = None
+
+            elif self.choice == "7":
+                self.tcp_socket.connect((self.server_name, self.server_port))
+                colored_print("Chat Room Client Started", "success")
+                while True:
+                    self.update_peers()
+                    if not self.room_peers:
+                        break
+                    message_to_send = input(colored_print("You: ", "prompt"))
+                    self.update_peers()
---
+++ Network/PeerServer.py
@@ -0,0 +1,119 @@
+from socket import *
+import logging
+import threading
+import select
+
+from Service.color_utils import colored_print
+from Service.password import Password
+from DAO.database_access import DatabaseAccess
+import colorama
+
+
+class PeerServer(threading.Thread):
+    '''
+    This class is used to process the server side of the peer-to-peer connection.
+    '''
+
+    def __init__(self, username, peer_server_port, chat_room_server_port):
+        '''
+        Peer Server Initialization
+        '''
+        threading.Thread.__init__(self)
+        self.username = username
+        self.peer_server_port = peer_server_port
+        self.chat_room_server_port = chat_room_server_port
+        self.tcp_server_socket = socket(AF_INET, SOCK_STREAM)
+        self.udp_server_socket = socket(AF_INET, SOCK_DGRAM)
+
+        self.isChatting = False # Flag to check if the user is chatting
+        self.isOnline = True # Flag to check if the user is online
+
+        self.connected_peer_ip = None # IP address of the connected peer
+        self.connected_peer_port = None # Port number of the connected peer
+        self.connected_peer_socket = None # Socket of the connected peer
+        self.connected_peer_username = None
+        self.chat = 0
+        self.room = 0
+
+    def run(self) -> None:
+
+        colored_print("Peer server started", "success")
+
+        hostname = gethostname()
+        try:
+            self.peer_server_hostname = gethostbyname(hostname)
+        except gaierror:
+            colored_print("Couldn't get host name", "error")
+            return
+
+        self.udp_server_socket.bind(("192.168.1.107", self.chat_room_server_port))
+        self.tcp_server_socket.bind((self.peer_server_hostname, self.peer_server_port))
+
+        self.tcp_server_socket.listen(4)
+
+        sockets = [self.tcp_server_socket, self.udp_server_socket]
+
+        while sockets and self.isOnline:
+            try:
+                read_sockets, write_sockets, error_sockets = select.select(sockets, [], [])
+                for read_socket in read_sockets:
+                    if read_socket == self.tcp_server_socket and not self.room:
+                        tcp_socket, address = self.tcp_server_socket.accept()
+                        tcp_socket.setblocking(0) # non-blocking socket
+                        logging.info("Received connection from " + str(address))
+                        sockets.append(tcp_socket)
+                        if not self.isChatting:
+                            colored_print(self.username + " is now connected with " + str(address), "success")
+                            self.connected_peer_ip = address[0]
+                            self.connected_peer_socket = tcp_socket
+                    elif read_socket == self.udp_server_socket and self.room == 1: # handling udp messages
+                        while True:
+                            message, address = self.udp_server_socket.recvfrom(1024)
+                            logging.info("Received message: " + message.decode() + " from " + str(address))
+                            if not self.room:
+                                break
+                    elif not self.room:
+                        # communication with the peer
+                        message = read_socket.recv(1024).decode()
+                        logging.info("Received message: " + str(message) + " from " + str(self.connected_peer_ip))
+                        if str(message).split()[0].lower() == "create-private-chat-request":
+                            if read_socket == self.connected_peer_socket:
+                                message = message.split()
+                                self.connected_peer_port = message[2]
+                                self.connected_peer_username = message[1]
+                                colored_print("Private chat request from " + self.connected_peer_username, "info")
+                                colored_print("Type 'accept' to accept the request or 'reject' to reject it", "prompt")
+                                self.isChatting = True
+                            elif read_socket is not self.connected_peer_socket and self.isChatting:
+                                response_message = "reject-connection " + message.split()[1] + " " + message.split()[2]
+                                read_socket.send(response_message.encode())
+                                sockets.remove(read_socket)
+                        elif str(message).split()[0].lower() == "accept-connection":
+                            self.isChatting = True
+                        elif str(message).split()[0].lower() == "reject-connection":
+                            self.isChatting = False
+                            sockets.remove(read_socket)
+                        elif len(str(message)) > 0:
+                            # received a message from the peer
+                            colored_print(self.connected_peer_username + ": " + message, "menu")
+                        elif str(message).split()[0].lower() == "end-connection":
+                            colored_print("Connection ended with " + self.connected_peer_username, "info")
+                            if self.room:
+                                self.room = 0
+                            else:
+                                self.isChatting = False
+                                sockets.clear()
+                                sockets.append(self.tcp_server_socket)
+                        elif len(str(message)) == 0:
+                            colored_print("Connection ended with " + self.connected_peer_username, "info")
+                            self.isChatting = False
+                            sockets.clear()
+                            sockets.append(self.tcp_server_socket)
+            except OSError as e:
+                colored_print("Error: " + str(e), "error")
+                break
+
+
+
+
+
